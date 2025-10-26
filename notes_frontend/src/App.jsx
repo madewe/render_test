e6cf9563ef451c13240fc429fe8e5cbb865f9dcc
madewe <mademade@duck.com>
2025-10-26 02:16:41 +0100

    Produktiv-Code des Frontend-Teils erstellt und in Root des Backend-Teils integriert, kleinere Anpassungen wie relativer Link zur BaseUrl und statische Auslieferung durch Express

--- notes_frontend/src/App.jsx
+++ notes_frontend/src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
   const notesToShow = showAll ? notes : notes.filter(note => note.important);
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3002/api/notes/${id}`;
+    const url = `/api/notes/${id}`;
     const note = notes.find(n => n.id === id);
     const changedNote = {...note, important: !note.important}
 
